978fd34864e2f5d35c1c8be155075f40ec3a611c
KobeGuo99 <kobe.h.guo@vanderbilt.edu>
2023-02-06 06:29:23 -0600

    finished design of login and signup buttons

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,7 +1,9 @@
-import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react';
+import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Flex } from '@chakra-ui/react';
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
+import AuthInputs from './Authinputs';
+
 
 const AuthModal: React.FC = () => {
     const [modalState, setModalState] = useRecoilState(authModalState)
@@ -17,10 +19,29 @@ const AuthModal: React.FC = () => {
             <Modal isOpen={modalState.open} onClose={handleClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalHeader textAlign='center'>
+                        {modalState.view === 'login' && 'Login'}
+                        {modalState.view === 'signup' && 'Sign Up'}
+                        {modalState.view === 'resetPassword' && 'Reset Password'}
+                    </ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody>
-                        Here is the modal body
+                    <ModalBody 
+                        display='flex' 
+                        flexDirection='column' 
+                        alignItems='center'
+                        justifyContent='center'
+                        pb={6}
+                    >
+                        <Flex 
+                            direction='column' 
+                            align='center' 
+                            justify='center'
+                            width='70%'
+                        >
+                            {/* <OAuthButtons /> */}
+                            <AuthInputs />
+                            {/* <ResetPassword /> */}
+                        </Flex>
                     </ModalBody>
                 </ModalContent>
             </Modal>
