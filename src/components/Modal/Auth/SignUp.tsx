ebe87a54b12815d34e2375adde9d80a0f404bb69
KobeGuo99 <kobe.h.guo@vanderbilt.edu>
2023-02-08 04:20:01 -0600

    worked on firebase on sign up

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -1,11 +1,12 @@
-import { authModalState } from '@/src/atoms/authModalAtom';
+import { authModalState } from '../../../atoms/authModalAtom';
 import { Input, Button, Flex, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { auth } from '../../../firebase/clientApp'
 
+const SignUp: React.FC = () => {
 
-const SignUp:React.FC = () => {
-    
     const setAuthModalState = useSetRecoilState(authModalState)
 
     const [signUpForm, setSignUpForm] = useState({
@@ -14,8 +15,33 @@ const SignUp:React.FC = () => {
         confirmPassword: '',
     })
 
+    const [error, setError] = useState('')
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        userError,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
     // Firebase logic
-    const onSubmit = () => { }
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (error) setError('');
+
+        if (signUpForm.password !== signUpForm.confirmPassword) {
+            // set error
+            setError('Passwords do not match');
+            return;
+        }
+        
+        if (signUpForm.password.length <= 6) {
+            setError('Password must be at least 6 characters long');
+            return;
+        }
+        //passwords match
+        createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
+    };
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         //update form state
@@ -91,20 +117,26 @@ const SignUp:React.FC = () => {
                 }}
                 bg='gray.50'
             />
+            {error && (
+                <Text textAlign='center' color='red' fontSize='10pt'>
+                    {error}
+                </Text>
+            )}
             <Button
                 width='100%'
                 height='36px'
                 mt={2}
                 mb={2}
                 type='submit'
+                isLoading={loading}
             >
                 Sign Up
             </Button>
             <Flex fontSize='9pt' justifyContent='center'>
                 <Text mr={1}>Already a redditor?</Text>
-                <Text 
-                    color='blue.500' 
-                    fontWeight={700} 
+                <Text
+                    color='blue.500'
+                    fontWeight={700}
                     cursor='pointer'
                     onClick={() => setAuthModalState(prev => ({
                         ...prev,
@@ -113,7 +145,7 @@ const SignUp:React.FC = () => {
                 >
                     LOG IN
                 </Text>
-                
+
             </Flex>
         </form>
     )
